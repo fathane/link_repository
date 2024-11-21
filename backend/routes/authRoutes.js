be01fc38f9d2edb0ef1f8d8db279658d1dbd3890
fathane <wilfred.marcos@epitech.eu>
2024-11-21 23:17:22 +0100

    commit progsql

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -16,28 +16,26 @@ router.post('/login', [
 
     const { email, password } = req.body;
     try {
-        db.query('SELECT * FROM users WHERE email = ?', [email], async (err, results) => {
-            if (err) {
-                console.error('Erreur lors de la connexion', err);
-                return res.status(500).json({ error: 'Erreur lors de la connexion' });
-            }
-            if (results.length === 0) {
-                return res.status(401).json({ error: 'Email ou mot de passe incorrect' });
-            }
+        const query = 'SELECT * FROM users WHERE email = $1';
+        const result = await db.query(query, [email]);
 
-            const user = results[0];
-            const isMatch = await bcrypt.compare(password, user.password); // Comparaison du mot de passe
+        if (result.rows.length === 0) {
+            return res.status(401).json({ error: 'Email ou mot de passe incorrect' });
+        }
 
-            if (!isMatch) {
-                return res.status(401).json({ error: 'Email ou mot de passe incorrect' });
-            }
+        const user = result.rows[0];
+        const isMatch = await bcrypt.compare(password, user.password);
 
-            const token = jwt.sign({ userId: user.id }, 'SECRET_KEY', { expiresIn: '1h' });
-            req.session.token = token; // Stocker le token dans la session
-            res.json({ token });
-        });
+        if (!isMatch) {
+            return res.status(401).json({ error: 'Email ou mot de passe incorrect' });
+        }
+
+        const token = jwt.sign({ userId: user.id }, 'SECRET_KEY', { expiresIn: '1h' });
+        req.session.token = token; // Stocker le token dans la session
+        res.json({ token });
     } catch (error) {
-        res.status(500).send({ error: 'Erreur lors de la connexion' });
+        console.error('Erreur lors de la connexion', error);
+        res.status(500).json({ error: 'Erreur lors de la connexion' });
     }
 });
 
