2cccbfbe019d843e0eb496b6745e2d5febfa3690
fathane <wilfred.marcos@epitech.eu>
2024-11-21 20:16:14 +0100

    stock

--- frontend/src/productsData.js
+++ frontend/src/productsData.js
@@ -8,6 +8,7 @@ const products = [
         category: 'electronic',
         imgSrc: './image/vert.avif',
         videoSrc: './video/video.mp4',
+        stock: "illimite",
         description: '✅Un plat de <span style="color: red;">spaghettis</span> savoureux avec sauce maison',
         modalDescription: 'Our special spaghetti with fresh ingredients and homemade sauce\n Our special spaghetti with fresh ingredients and homemade sauce\n Our special spaghetti with fresh ingredients and homemade sauce\n Our special spaghetti with fresh ingredients and homemade sauce\nOur special spaghetti with fresh ingredients and homemade sauce'
     },
@@ -17,6 +18,7 @@ const products = [
         price: 15000,
         originalPrice: 25000,
         category: 'traditionnel',
+        stock: "limité",
         description: 'Nike Aire max pointure valide\n Pointure 37-49',
         modalDescription: '✅ Nike Aire max pointure valide\n ✅ Pointure 37-49',
         imgSrc: './image/nike.jpg',
