d8539d3716189fe36f97175220dcf4bc4e99ad80
fathane <wilfred.marcos@epitech.eu>
2025-02-15 05:46:47 +0100

    code

--- frontend/src/components/CartPage.js
+++ frontend/src/components/CartPage.js
@@ -9,7 +9,6 @@ function CartPage({ cart, onRemoveFromCart }) {
     const [isPromoValid, setIsPromoValid] = useState(null);
 
     const validPromoCodes = {
-        "ST_VAL_02": 0.9,
         "FREESHIP": 1,
         "RCY_01": 0.9,
         "4KULTURE_4": 0.9,
